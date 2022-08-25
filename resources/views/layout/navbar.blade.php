54fc655ec5f98391154face1dbb8d692a4814fd1
Karan Sanjeev <yo@alphaman.me>
2022-08-26 03:52:22 +0530

    Work on dashboard migration

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class='navbar'>
+    <div class='container'>
+        <div class='navbar-brand'>
+            <a href='#' class='navbar-item is-size-4'>Amar-Raksha</a>
+        </div>
+        <div class='navbar-menu'>
+            <div class='navbar-end'>
+                <a class='navbar-item'>Dashboard</a>
+                <a class='navbar-item'>Events</a>
+                <a class='navbar-item'>Blogs</a>
+                <a class='navbar-item'>Notifications</a>
+                <div class='navbar-item'>
+                    <a class='button is-danger is-outlined'>Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::view('/', 'welcome');
+Route::view('login', 'auth.login');
+Route::view('admin/dashboard', 'admin.dashboard');
+Route::view('institutions/dashboard', 'institutions.dashboard');
+Route::view('volunteers/dashboard', 'volunteers.dashboard');
